5c122dd755317eb55dd3978eb15b6662f452eb07
Hubert Wyszyński <hubertwyszynski@MacBook-Air-Hubert.local>
2020-01-02 13:22:52 +0100

    Updates

---
+++ precache-manifest.419cc39fd7845cf9f8d2ad571dea5135.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5be5cb1abe56f6b618425c2865efc75",
+    "url": "/notes-app/index.html"
+  },
+  {
+    "revision": "8d39c842e82908fedab0",
+    "url": "/notes-app/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "9477f29d65076b412532",
+    "url": "/notes-app/static/css/main.077afb28.chunk.css"
+  },
+  {
+    "revision": "8d39c842e82908fedab0",
+    "url": "/notes-app/static/js/2.e91a897a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/notes-app/static/js/2.e91a897a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9477f29d65076b412532",
+    "url": "/notes-app/static/js/main.19ea8dcc.chunk.js"
+  },
+  {
+    "revision": "f7ce01ab1fabbdfba237",
+    "url": "/notes-app/static/js/runtime-main.68f5a76e.js"
+  },
+  {
+    "revision": "9ab31f5611cb61b0dc7ae5415da85c6f",
+    "url": "/notes-app/static/media/desktop.9ab31f56.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/notes-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/notes-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/notes-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/notes-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/notes-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
